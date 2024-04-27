fc7ee8dbc78cde75e9987b15e8e99ed53f29f3f8
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-04-27 21:08:48 +0600

    added put data on clint section

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,18 +11,55 @@ app.use(express.json());
 
 
 
+const uri =
+  `mongodb+srv://${process.env.DB_user}:${process.env.DB_password}@cluster0.hzcboi3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri)
 
-app.get('/', (req, res) => {
-    res.send('ARET & CRAFT SERVER IS RUNNING')
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      
+      app.post('/craft', async (req, res) => {
+          const newCraft = req.body;
+          console.log(newCraft)
 })
 
 
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("ARET & CRAFT SERVER IS RUNNING");
+});
 
 app.listen(port, () => {
-    console.log(`ARET & CRAFT SERVER IS RUNNING: ${port}`)
-})
+  console.log(`ARET & CRAFT SERVER IS RUNNING: ${port}`);
+});
 
-
-
-app.get
-
+app.get("/");
